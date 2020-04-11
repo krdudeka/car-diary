4a49ab0205b7e38a5dbda3ca59a222c4d5d5b22a
krdudeka <krdudeka@wszib.edu.pl>
2020-04-11 18:03:31 +0200

    Initial commit

---
+++ src/main/java/pl/edu/wszib/project/cardiary/CarDiaryApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.project.cardiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarDiaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarDiaryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/edu/wszib/project/cardiary/CarDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.project.cardiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarDiaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
